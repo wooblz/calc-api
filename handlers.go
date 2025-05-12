fe9d561940336f9ca7dcbf7ff55ef57e6fb59f09
Ethan Kim <ethank0626@gmail.com>
2025-05-12 15:46:03 -0700

    basic 4 requests

--- handlers.go
+++ handlers.go
@@ -11,7 +11,14 @@ func (s server) ServeHTTP(w http.ResponseWriter, r *http.Request)  {
     switch r.URL.Path  {
     case "/add":
         s.AddHandler(w,r)
+    case "/subtract":
+        s.SubtractHandler(w,r)
+    case "/divide":
+        s.DivisionHandler(w,r)
+    case "/multiply":
+        s.MultiplicationHandler(w,r)
     }
+
 }
 func (s server) AddHandler(w http.ResponseWriter, r *http.Request)  {
     w.Header().Set("Content-Type", "application/json")
@@ -51,13 +58,159 @@ func (s server) AddHandler(w http.ResponseWriter, r *http.Request)  {
         })
         return
     }
+    w.WriteHeader(http.StatusOK)
     w.Write(jsonBytes)
     logger.Info(string(jsonBytes),
         "path",r.URL.Path,
         "ip", r.RemoteAddr,
     )
 }
+func (s server) SubtractHandler(w http.ResponseWriter,r *http.Request)  {
+    w.Header().Set("Content Type", "application/json")
+    logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+    defer r.Body.Close()
 
+    jsonDecoder := json.NewDecoder(r.Body)
+    
+    calcReq := TwoValues{}
+    err := jsonDecoder.Decode(&calcReq)
+    if err != nil  {
+        logger.Error("Failed to decode JSON", 
+            "path", r.URL.Path,
+            "ip", r.RemoteAddr,
+            "error", err.Error(),
+        )
+        w.WriteHeader(http.StatusBadRequest) 
+        json.NewEncoder(w).Encode(map[string]string  {
+            "error": "Invalid JSON input",
+        })
+        return
+    }
+
+    solution := SingleValue{}
+    solution.Value1 = calcReq.Value1 - calcReq.Value2
+
+    jsonBytes, err := json.Marshal(solution)
+    if err != nil  {
+        logger.Error("Failed encoding",
+            "path", r.URL.Path,
+            "ip", r.RemoteAddr,
+            "error", err.Error(),
+        )
+        w.WriteHeader(http.StatusInternalServerError)
+        json.NewEncoder(w).Encode(map[string]string  {
+            "error": "Failed encoding",
+        })
+        return
+    }
+    w.WriteHeader(http.StatusOK)
+    w.Write(jsonBytes)
+    logger.Info(string(jsonBytes),
+        "path",r.URL.Path,
+        "ip", r.RemoteAddr,
+    )
+}
+func (s server) DivisionHandler(w http.ResponseWriter,r *http.Request)  {
+    w.Header().Set("Content Type", "application/json")
+    logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+    defer r.Body.Close()
+
+    jsonDecoder := json.NewDecoder(r.Body)
+    
+    calcReq := TwoValues{}
+    err := jsonDecoder.Decode(&calcReq)
+    if err != nil  {
+        logger.Error("Failed to decode JSON", 
+            "path", r.URL.Path,
+            "ip", r.RemoteAddr,
+            "error", err.Error(),
+        )
+        w.WriteHeader(http.StatusBadRequest) 
+        json.NewEncoder(w).Encode(map[string]string  {
+            "error": "Invalid JSON input",
+        })
+        return
+    }
+    if calcReq.Value2 == 0  {
+        logger.Error("Invalid Divisor",
+            "path", r.URL.Path,
+            "ip", r.RemoteAddr,
+            "error", err.Error(),
+        )
+        w.WriteHeader(http.StatusBadRequest) 
+        json.NewEncoder(w).Encode(map[string]string  {
+            "error": "Cannot divide by 0",
+        })
+        return
+    }
+    solution := SingleValue{}
+    solution.Value1 = calcReq.Value1/calcReq.Value2
+
+    jsonBytes, err := json.Marshal(solution)
+    if err != nil  {
+        logger.Error("Failed encoding",
+            "path", r.URL.Path,
+            "ip", r.RemoteAddr,
+            "error", err.Error(),
+        )
+        w.WriteHeader(http.StatusInternalServerError)
+        json.NewEncoder(w).Encode(map[string]string  {
+            "error": "Failed encoding",
+        })
+        return
+    }
+    w.WriteHeader(http.StatusOK)
+    w.Write(jsonBytes)
+    logger.Info(string(jsonBytes),
+        "path",r.URL.Path,
+        "ip", r.RemoteAddr,
+    )
+}
+func (s server) MultiplicationHandler(w http.ResponseWriter,r *http.Request)  {
+    w.Header().Set("Content Type", "application/json")
+    logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+    defer r.Body.Close()
+
+    jsonDecoder := json.NewDecoder(r.Body)
+    
+    calcReq := TwoValues{}
+    err := jsonDecoder.Decode(&calcReq)
+    if err != nil  {
+        logger.Error("Failed to decode JSON", 
+            "path", r.URL.Path,
+            "ip", r.RemoteAddr,
+            "error", err.Error(),
+        )
+        w.WriteHeader(http.StatusBadRequest) 
+        json.NewEncoder(w).Encode(map[string]string  {
+            "error": "Invalid JSON input",
+        })
+        return
+    }
+
+    solution := SingleValue{}
+    solution.Value1 = calcReq.Value1 * calcReq.Value2
+
+    jsonBytes, err := json.Marshal(solution)
+    if err != nil  {
+        logger.Error("Failed encoding",
+            "path", r.URL.Path,
+            "ip", r.RemoteAddr,
+            "error", err.Error(),
+        )
+        w.WriteHeader(http.StatusInternalServerError)
+        json.NewEncoder(w).Encode(map[string]string  {
+            "error": "Failed encoding",
+        })
+        return
+    }
+    w.WriteHeader(http.StatusOK)
+    w.Write(jsonBytes)
+    logger.Info(string(jsonBytes),
+        "path",r.URL.Path,
+        "ip", r.RemoteAddr,
+    )
+}
 func main()  {
     log.Fatal(http.ListenAndServe(":8080",server{}))
 }
